27813602d07d550599f575d7812f7337d5f3214f
Alexandr <87609566+MethanolCode@users.noreply.github.com>
2024-03-05 23:43:54 +0000

    Commit1

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ def start(message):
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.reply_to(message, "/FuckNiggers - fucking niggers \n")
+    bot.reply_to(message, "FuckNiggers - fucking niggers \n")
 
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-#@bot.message_handler(func=lambda message: True)
-#def echo_message(message):
-#    bot.reply_to(message, message.text)
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 
 bot.infinity_polling(none_stop=True)
